1565fb7c863ac1c0f4756193b2faf25886e68303
NeoExperiences <70493616+NeoExperiences@users.noreply.github.com>
2022-06-21 17:06:49 -0300

    Creacion del Backend Actualizada.

--- backend/db/database.sql
+++ backend/db/database.sql
@@ -1,54 +1,59 @@
-CREATE DATABASE IF NOT EXISTS devplaceworkshop;
+CREATE DATABASE IF NOT EXISTS 1proyectoblog;
 
-use devplaceworkshop;
+use 1proyectoblog;
 
 CREATE TABLE IF NOT EXISTS roles (
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(64) NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS usuarios (
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(64) NOT NULL UNIQUE,
     password VARCHAR(128) NOT NULL,
     email VARCHAR(128) NOT NULL UNIQUE,
     address VARCHAR(1024),
+	picture VARCHAR(1024),
     role INT UNSIGNED,
     FOREIGN KEY (role) REFERENCES roles(id)
 );
 
-CREATE TABLE IF NOT EXISTS categories (
+CREATE TABLE IF NOT EXISTS categorias (
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(64) NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS products (
-    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(64) NOT NULL UNIQUE,
-    price FLOAT NOT NULL,
-    stock INT UNSIGNED,
-    category INT UNSIGNED NOT NULL,
-    FOREIGN KEY (category) REFERENCES categories(id)
+CREATE TABLE IF NOT EXISTS articulos (
+  id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  postName VARCHAR(256) NOT NULL UNIQUE,
+  postContent VARCHAR(1024) NOT NULL,
+  postCategory INT UNSIGNED NOT NULL,
+  postDate varchar(256) NOT NULL,
+  userID INT UNSIGNED NOT NULL,
+  picture varchar(256) NOT NULL
+  FOREIGN KEY (postCategory) REFERENCES categorias(id)
+  FOREIGN KEY (userID) REFERENCES usuarios(id)
 );
 
-CREATE TABLE IF NOT EXISTS states (
-    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(64) NOT NULL UNIQUE
+CREATE TABLE IF NOT EXISTS comentarios (
+  id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  userComment varchar(256) NOT NULL,
+  commentDate varchar(256) NOT NULL,
+  userID int(11) NOT NULL,
+  postID int(11) NOT NULL
+  FOREIGN KEY (userID) REFERENCES usuarios(id)
+  FOREIGN KEY (postID) REFERENCES articulos(id)
 );
 
-CREATE TABLE IF NOT EXISTS orders (
-    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    userId INT UNSIGNED,
-    status INT UNSIGNED,
-    FOREIGN KEY (status) REFERENCES states(id),
-    FOREIGN KEY (userId) REFERENCES users(id)
+CREATE TABLE IF NOT EXISTS respuestas (
+  id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  userReply varchar(256) NOT NULL,
+  replyDate varchar(256) NOT NULL,
+  userID int(11) NOT NULL,
+  commentID int(11) NOT NULL,
+  postID int(11) NOT NULL
+  FOREIGN KEY (userID) REFERENCES usuarios(id)
+  FOREIGN KEY (postID) REFERENCES articulos(id)
+  FOREIGN KEY (commentID) REFERENCES comentarios(id)
 );
 
-CREATE TABLE IF NOT EXISTS orders_products (
-    orderId INT UNSIGNED,
-    productId INT UNSIGNED,
-    amount INT UNSIGNED,
-    PRIMARY KEY (orderId, productId),
-    FOREIGN KEY (orderId) REFERENCES orders(id),
-    FOREIGN KEY (productId) REFERENCES products(id)
-);
--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -2,11 +2,28 @@ INSERT INTO roles (id, name) VALUES
     (1, "admin"),
     (2, "client");
 
-INSERT INTO categories (id, name) VALUES
-    (1, "tools"),
-    (2, "food");
+INSERT INTO `categorias` (`id`, `name`) VALUES
 
-INSERT INTO states (id, name) VALUES
-    (1, "pending"),
-    (2, "processed"),
-    (3, "delivered");
+(1, 'Info'),
+(2, 'Juegos'),
+(3, 'Juegos On-Line'),
+(4, 'Links'),
+(5, 'Linux'),
+(6, 'Mac'),
+(7, 'Manga'),
+(8, 'Mascotas'),
+(9, 'Musica'),
+(10, 'Noticias'),
+(11, 'Off-Topic'),
+(12, 'Paranormal'),
+(13, 'Patrocinados'),
+(14, 'Recetas y Cocina'),
+(15, 'Reviews'),
+(16, 'Salud y Bienestar'),
+(17, 'Solidaridad'),
+(18, 'Arte'),
+(19, 'Turismo'),
+(20, 'T.V. Películas y Series');
+
+INSERT INTO `usuarios` (`id`, `username`, `password`, `email`, `address`, `role`) VALUES
+(1, 'Neo', '$2b$10$7Kn8uLrSTkskDztVhTrSK.dB0mXlL.MWwtBDLW/e0JphKu9BzaJAe', 'Test@Mail.com', 'Lorem St. 540', 1),
